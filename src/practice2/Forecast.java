972f766bbd3888aa8595963b715bf26b7e353ab7
yxy <yxy2002@njfu.edu.cn>
2022-11-26 16:03:31 +0800

    Initial commit

--- src/practice2/Forecast.java
+++ src/practice2/Forecast.java
@@ -30,24 +30,35 @@ public class Forecast {
                 forecast[i][j] = "";
             }
         for (int i = 1; i < forecast.length; i++) {
-            forecast[i][0] = String.valueOf(x.charAt(i - 1));
+            forecast[i][0] = String.valueOf(y.charAt(i - 1));
         }
         for (int j = 1; j < forecast[0].length; j++) {
-            forecast[0][j] = String.valueOf(y.charAt(j - 1));
+            forecast[0][j] = String.valueOf(x.charAt(j - 1));
         }
         /*
         传入的文法已将同样非终止符开始的文法合并在一起
         方便后面First集的运算
          */
-        String[] grammar = new String[5];
+        String[] grammar = new String[10];
         grammar[0] = "E->TG";
-        grammar[1] = "G->+TG|-TG|ε";
-        grammar[2] = "T->FS";
-        grammar[3] = "S->*FS|/FS|ε";
-        grammar[4] = "F->(E)|i";
+        grammar[1] = "G->+TG";
+        grammar[2] = "G->-TG";
+        grammar[3] = "G->ε";
+        grammar[4] = "T->FS";
+        grammar[5] = "S->*FS";
+        grammar[6] = "S->/FS";
+        grammar[7] = "S->ε";
+        grammar[8] = "F->(E)|i";
+        grammar[9] = "F->i";
 
         String terminator = "+-*/()iε";
         String not_terminator = "EGTSF";
+        //求出所有First集存储在字符数组中
+        String[] first = new String[not_terminator.length()];
+        for (int i = 0; i < not_terminator.length(); i++) {
+            first[i] = First(not_terminator.charAt(i), terminator, grammar);
+            System.out.println("First(" + not_terminator.charAt(i) + "):" + first[i]);
+        }
 
         show(forecast);
 
@@ -74,32 +85,34 @@ public class Forecast {
     }
 
     //求First集
-    public static String First(char ch, String terminator, String not_terminator, String[] grammar) {
+    public static String First(char ch, String terminator, String[] grammar) {
         //找到ch对应的文法用s表示
         String s = "";
-        for (int i = 0; i < grammar.length; i++) {
-            if (grammar[i].charAt(1) == ch) {
-                s = grammar[i];
-            }
-        }
-        String str = "";
-        //找到->后面跟着的符号,判断是不是终止符
-        if (terminator.contains(String.valueOf(s.charAt(3)))) {
-            str = str + s.charAt(3);
-        } else {
-            String tempString = First(s.charAt(3), terminator, not_terminator, grammar);
-            //检查该First集中是否含有空字符
-            int i=3;//记录charAt的下标 相当于X->Y1Y2...YK中的下标
-            while (tempString.contains("ε")) {
-                i++;
-                tempString = tempString.replace("ε", "");
-                if(s.charAt(i)=='|'||s.charAt(i)=='\"'){
-                    tempString=tempString+"ε";
+        StringBuilder str = new StringBuilder();
+        for (String value : grammar) {
+            if (value.charAt(0) == ch) {
+                s = value;
+                //找到->后面跟着的符号,判断是不是终止符再试图找到
+                if (terminator.contains(String.valueOf(s.charAt(3)))) {
+                    str.append(s.charAt(3));
+                } else {
+                    String tempString = First(s.charAt(3), terminator, grammar);
+                    //检查该First集中是否含有空字符
+                    int i = 3;//记录charAt的下标 相当于X->Y1Y2...YK中的下标
+                    while (tempString.contains("ε")) {
+                        i++;
+                        tempString = tempString.replace("ε", "");
+                        if (s.charAt(i) == '|' || s.charAt(i) == '\"') {
+                            tempString = tempString + "ε";
+                        } else {
+                            tempString = tempString + First(s.charAt(i), terminator, grammar);
+                        }
+                    }
+                    str.append(tempString);
                 }
             }
-            str = str + tempString;
         }
-        return str;
+        return str.toString();
     }
 
 
